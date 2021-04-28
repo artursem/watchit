9ec0044c631cfcd7b02b5e2589f84e31bdc176d5
artursem <artur.semeniuk@gmail.com>
2021-04-28 19:40:38 +0200

    add fs check

--- index.js
+++ index.js
@@ -2,6 +2,25 @@
 
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
+const program = require('caporal');
+const fs = require('fs');
+
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action(async ({ filename }) => {
+        const name = filename || 'index.js';
+
+        try {
+            await fs.promises.access(name);
+        } catch (err) {
+            throw new Error(`Could not find the file ${name}`);
+        }
+
+        console.log(args);
+    });
+
+program.parse(process.argv);
 
 const start = debounce(() => {
     console.log('STARTING USER\'S PROGRAM');
@@ -10,5 +29,5 @@ const start = debounce(() => {
 chokidar
     .watch('.')
     .on('all', start)
-    .on('change', () => console.log('FILE CHANGED'))
-    .on('unlinked', () => console.log('FILE UNLINKED'));
+    .on('change', start)
+    .on('unlinked', start);
