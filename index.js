caf901264a2226d042fc7ab38375221f660350d6
artursem <artur.semeniuk@gmail.com>
2021-04-28 19:07:20 +0200

    add chokidar

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('i was executed!');
+const chokidar = require('chokidar');
+
+chokidar
+    .watch('.')
+    .on('all', () => console.log('FILE ADDED'))
+    .on('change', () => console.log('FILE CHANGED'))
+    .on('unlinked', () => console.log('FILE UNLINKED'));
