39beb52a8a93ffa5c8439099fcde532c11749e26
artursem <artur.semeniuk@gmail.com>
2021-04-28 19:16:08 +0200

    add debounce

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+
+const start = debounce(() => {
+    console.log('STARTING USER\'S PROGRAM');
+}, 100);
 
 chokidar
     .watch('.')
-    .on('all', () => console.log('FILE ADDED'))
+    .on('all', start)
     .on('change', () => console.log('FILE CHANGED'))
     .on('unlinked', () => console.log('FILE UNLINKED'));
